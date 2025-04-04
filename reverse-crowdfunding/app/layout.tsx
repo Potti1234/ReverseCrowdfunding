c60b48f52dd03d0dc898bb6c12849172275b4376
Potti1234 <lukas.pottner@gmail.com>
2025-04-04 20:14:21 +0200

    Add verification so only verified users can vote

--- reverse-crowdfunding/app/layout.tsx
+++ reverse-crowdfunding/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { BottomNavigation } from '@/components/bottom-navigation'
+import MiniKitProvider from '@/components/minikit-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,12 +19,14 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <div className='flex flex-col min-h-screen max-w-md mx-auto border-x'>
-          <main className='flex-1 pb-16'>{children}</main>
-          <BottomNavigation />
-        </div>
-      </body>
+      <MiniKitProvider>
+        <body className={inter.className}>
+          <div className='flex flex-col min-h-screen max-w-md mx-auto border-x'>
+            <main className='flex-1 pb-16'>{children}</main>
+            <BottomNavigation />
+          </div>
+        </body>
+      </MiniKitProvider>
     </html>
   )
 }
