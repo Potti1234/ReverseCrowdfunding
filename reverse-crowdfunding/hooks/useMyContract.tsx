6d27e64845136292d0ecffea08755ee90ad4c55a
Zihao <atjohore@gmail.com>
2025-04-06 05:30:02 +0800

    [fix]useMyContract.tsx

--- reverse-crowdfunding/hooks/useMyContract.tsx
+++ reverse-crowdfunding/hooks/useMyContract.tsx
@@ -8,10 +8,11 @@ const CONTRACT_ADDRESS = "0xDB611E19303debA0C967A6f293E23Fc5D9D58513";
 
 const typedABI = CrowdfundingContract.abi as InterfaceAbi;
 
-export const useMyContract = (signer: unknown) => {
+export const useMyContract = (signer: unknown | void) => {
   const contract = useMemo(() => {
+    const provider = signer ? signer : worldcoinProvider;
     return new Contract(CONTRACT_ADDRESS, typedABI, worldcoinProvider);
-  }, []);
+  }, [signer]);
 
   return contract;
 };
